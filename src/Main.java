fcf2104da1d84cf1e7388095368167b95e107978
Serdyukov <and.serdykov@gmail.com>
2022-09-03 13:02:51 +0300

    Выполненые задания №1, №2, №3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ import java.time.LocalDate;
 import java.util.Locale;
 
 public class Main {
+
+    public static void main(String[] args) {
+        //Задание №1
+        int currentYear = 2020;
+        checkYear(currentYear);
+
+        //Задание №2
+        int clientOs = 0;
+        int clientDeviceYear = 2022;
+        getClientOs(clientOs, clientDeviceYear);
+
+        //Задание №3
+        int deliveryDistance = 87;
+        deliveryTime(deliveryDistance);
+    }
+
+    //Метод для задания №1
     public static void checkYear (int year){
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " - високосный год.");
@@ -10,6 +27,7 @@ public class Main {
         }
     }
 
+    //Метод для задания №2
     public static void getClientOs (int clientOs, int clientDeviceYear) {
         int currentYear = LocalDate.now().getYear();
         if (clientOs <= 1 && clientOs >= 0) {
@@ -25,16 +43,18 @@ public class Main {
         } else {
             System.out.println("Извините, ваша OS не поддерживается.");
         }
-
     }
-    public static void main(String[] args) {
-        //Задание №1
-        int currentYear = 2020;
-        checkYear(currentYear);
 
-        //Задание №2
-        int clientOs = 0;
-        int clientDeviceYear = 2022;
-        getClientOs(clientOs, clientDeviceYear);
+    //Метод для задания №3
+    public static void deliveryTime (int deliveryDistance) {
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется 1 день для доставки");
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется 2 дня для доставки");
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется 3 дня для доставки");
+        } else {
+            System.out.println("Потребуется помощь специалиста логистики");
+        }
     }
 }
